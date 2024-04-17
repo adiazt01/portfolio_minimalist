6096918da4c0d72e88a6d4150942e1401e0fdd40
adiazt01 <adiaztovar2004@gmail.com>
2024-04-17 14:02:37 -0400

    Update icon sizes in SVG files

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+// Social media icons
+export { default as Github } from "./Github.astro"
+export { default as Mail} from "./Mail.astro"
+export { default as Linkedin } from "./Linkedin.astro"
+
+// Icons for misc
+export { default as Link } from "./Link.astro"
