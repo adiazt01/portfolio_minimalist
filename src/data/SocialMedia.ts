24eff6af763c09c04828934e96975262ae239dd3
adiazt01 <adiaztovar2004@gmail.com>
2024-04-18 18:42:24 -0400

    Update index.astro and [id].astro files with new components and sections

---
+++ src/data/SocialMedia.ts
@@ -0,0 +1,19 @@
+import { Github, Linkedin, Mail } from "../components/icons";
+
+export const SOCIAL_MEDIA = [
+    {
+      network: "LinkedIn",
+      url: "https://www.linkedin.com/in/armando-diaz-291385251/",
+      icon: Linkedin,
+    },
+    {
+      network: "GitHub",
+      url: "https://github.com/adiazt01",
+      icon: Github,
+    },
+    {
+      network: "Mail",
+      url: "mailto:armandodt2004@gmail.com",
+      icon: Mail,
+    },
+  ];
