838ce1575d889d4f3ed181190ad8bd4b8b298067
adiazt01 <adiaztovar2004@gmail.com>
2024-04-18 20:34:19 -0400

    Update package.json, Certifications.ts, projects.json, index.astro, package-lock.json, Layout.astro, and Certificates.astro files

--- src/data/Certifications.ts
+++ src/data/Certifications.ts
@@ -2,6 +2,8 @@ export const CERTIFICATES = [
     {
       company: "FreeCodeCamp",
       name: "Responsive Web Design",
+      description: "This certification is for those who have completed the Responsive Web Design Certification on FreeCodeCamp.",
       url: "https://www.freecodecamp.org/certification/adiazt01/responsive-web-design",
+      date: "2022",
     },
   ];
