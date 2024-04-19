e3a13a56d586638bea24d7ab1640596c83a73f23
adiazt01 <adiaztovar2004@gmail.com>
2024-04-18 20:13:31 -0400

    Add Certifications data and Experience data files

---
+++ src/data/Certifications.ts
@@ -0,0 +1,7 @@
+export const CERTIFICATES = [
+    {
+      company: "FreeCodeCamp",
+      name: "Responsive Web Design",
+      url: "https://www.freecodecamp.org/certification/adiazt01/responsive-web-design",
+    },
+  ];
---
+++ src/data/Experience.ts
@@ -0,0 +1,12 @@
+export const WORK_EXPERIENCE = [
+    {
+      company: "Silvera Enterprises",
+      location: "Remoto",
+      startDate: "01/2024",
+      endDate: "03/2024",
+      companyUrl: "https://striveenterprise.com/es/",
+      position: "Desarrollador Front end",
+      description:
+        "Participando en el proyecto que permitio la creacion de una landing page usando WordPress, Costum Fields y ACF. Utilizando HTML, CSS, JavaScript y PHP para el desarrollo.",
+    },
+  ];
